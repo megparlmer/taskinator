8faf4b5fe3b2ba684adb9427da0e956036290e78
megparlmer <megparlmer@gmail.com>
2022-02-23 14:53:01 -0500

    disabled default browser behavior

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
-var buttonEl = document.querySelector("#save-task");
+//creating elements that will run when id is run
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+//the function that adds new task
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+
+    console.log(event);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+//a callback function
+formEl.addEventListener("submit", createTaskHandler);
